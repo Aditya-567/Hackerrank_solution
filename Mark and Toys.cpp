b59f73070d8d98d2bbb6d8d5dbdb61d20170871d
Aditya Kumar <106132841+Aditya-567@users.noreply.github.com>
2023-09-17 00:29:02 +0530

    Create Mark and Toys.cpp

---
+++ Mark and Toys.cpp
@@ -0,0 +1,48 @@
+// Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. 
+// There are a number of different toys lying in front of him, tagged with their prices. 
+// Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money.
+// Given a list of toy prices and an amount to spend, determine the maximum number of gifts he can buy.
+
+// Note Each toy can be purchased only once.
+
+// Example price = [1,2,3,4}
+// k=7
+
+// The budget is  units of currency. He can buy items that cost  for , or  for  units. The maximum is  items.
+
+// Function Description
+// Complete the function maximumToys in the editor below.
+// maximumToys has the following parameter(s):
+// int prices[n]: the toy prices
+// int k: Mark's budget
+
+// Returns
+// int: the maximum number of toys
+// Input Format
+// The first line contains two integers,  and , the number of priced toys and the amount Mark has to spend.
+// The next line contains  space-separated integers 
+
+// A toy can't be bought multiple times.
+// Sample Input
+
+// 7 50
+// 1 12 5 111 200 1000 10
+// Sample Output
+
+// 4
+
+
+int maximumToys(vector<int> prices, int k) {
+sort(prices.begin(), prices.end());
+    int count = 0;
+    int i = 0;
+    int sum=0;
+    while (sum<=k) {
+        sum+=prices[i];
+        i++;
+        count++;
+    }
+    return count-1;
+
+}
+    
